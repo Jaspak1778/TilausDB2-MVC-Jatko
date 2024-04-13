8307a2b1232eb2b4c34f3b7e9de5d557ae1fada1
Jani Pekkala <pekkala.jani@hotmail.fi>
2024-04-13 10:36:41 +0300

    jälleen rakennus

--- Controllers/TilausrivitController.cs
+++ Controllers/TilausrivitController.cs
@@ -37,25 +37,28 @@ namespace TilausDB2.Controllers
         }
 
         // GET: Tilausrivit/Create
-        public ActionResult Create()
+        public ActionResult Create(int? id)
         {
-            ViewBag.TilausID = new SelectList(db.Tilaukset, "TilausID", "Toimitusosoite");
+            Tilausrivit tilausrivit = new Tilausrivit();
+
+            tilausrivit.TilausID = id.GetValueOrDefault();
+
             ViewBag.TuoteID = new SelectList(db.Tuotteet, "TuoteID", "Nimi");
-            return View();
+            return View(tilausrivit);
+
         }
 
-        // POST: Tilausrivit/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "TilausriviID,TilausID,TuoteID,Maara,Ahinta")] Tilausrivit tilausrivit)
+        public ActionResult Create(int TilausID, [Bind(Include = "TilausID,TuoteID,Maara,Ahinta")] Tilausrivit tilausrivit)
         {
             if (ModelState.IsValid)
             {
+                                tilausrivit.TilausID = TilausID;
+
                 db.Tilausrivit.Add(tilausrivit);
                 db.SaveChanges();
-                return RedirectToAction("Index");
+                return RedirectToAction("Index", "Tilaukset");
             }
 
             ViewBag.TilausID = new SelectList(db.Tilaukset, "TilausID", "Toimitusosoite", tilausrivit.TilausID);
@@ -66,20 +69,27 @@ namespace TilausDB2.Controllers
         // GET: Tilausrivit/Edit/5
         public ActionResult Edit(int? TilausID, int? TuoteID)
         {
-            if (TilausID == null || TuoteID == null )
+            if (TilausID == null || TuoteID == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            /*Tilausrivit tilausrivit = db.Tilausrivit.Find(TilausID, TuoteID);*/
-            Tilausrivit tilausrivit = db.Tilausrivit.SingleOrDefault(tr => tr.TilausID == TilausID && tr.TuoteID == TuoteID); // ei ole PK niin..
-            if (tilausrivit == null)
+            
+            var tilausrivitList = db.Tilausrivit.Where(tr => tr.TilausID == TilausID && tr.TuoteID == TuoteID).ToList();
+
+            
+            if (tilausrivitList.Count == 0)
             {
                 return HttpNotFound();
             }
+
+            // For simplicity, let's just take the first element from the list
+            var tilausrivit = tilausrivitList.First();
+
             ViewBag.TilausID = new SelectList(db.Tilaukset, "TilausID", "Toimitusosoite", tilausrivit.TilausID);
             ViewBag.TuoteID = new SelectList(db.Tuotteet, "TuoteID", "Nimi", tilausrivit.TuoteID);
             return View(tilausrivit);
         }
+
 
         // POST: Tilausrivit/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
